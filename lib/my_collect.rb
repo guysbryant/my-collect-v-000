07c46444578cd1c7ee8e0fbea5fa2b8bd311031c
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-15 00:19:37 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 my_collection(list) do
   i = 0
   return_array = []
-  while i < list.length
-    yield return_array << list[i]
-    i += 1
+  if block_given?
+    while i < list.length
+      yield return_array << list[i]
+      i += 1
+    end
   end
   return_array
 end
